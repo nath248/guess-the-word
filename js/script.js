21622c51ac79b82fbfb55110bc27cfdffda12d1b
nath248 <nmherrera20@outlook.com>
2021-06-20 12:12:51 -0400

    Add global variables

---
+++ js/script.js
@@ -0,0 +1,9 @@
+const guessLetters = document.querySelector(".guessed-letters");
+const guessButton = document.querySelector(".guess");
+const letter = document.querySelector(".letter");
+const progress = document.querySelector(".word-in-progress");
+const remainingGuesses = document.querySelector(".remaining");
+const span = document.querySelector("span");
+const message = document.querySelector(".message");
+const hiddenButton = document.querySelector(".play-again hide");
+const word = "magnolia";
