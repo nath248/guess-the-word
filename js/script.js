ce1217b87214bb478336189a8acd5f62aa7a4066
nath248 <nmherrera20@outlook.com>
2021-06-20 14:15:44 -0400

    Add innerText

--- js/script.js
+++ js/script.js
@@ -3,7 +3,10 @@ const guessButton = document.querySelector(".guess");
 const letter = document.querySelector(".letter");
 const progress = document.querySelector(".word-in-progress");
 const remainingGuesses = document.querySelector(".remaining");
-const span = document.querySelector("span");
+const span = document.querySelector(".remaining span");
 const message = document.querySelector(".message");
-const hiddenButton = document.querySelector(".play-again hide");
+const hiddenButton = document.querySelector(".play-again");
 const word = "magnolia";
+
+progress.innerText = "●";
+progress(word);
